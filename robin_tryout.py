fd28a691ab28c2ce595333d5d25fc25410d4fe05
rvdb7345 <rvdb7345@gmail.com>
2022-08-24 14:26:44 +0200

    added most of the parameters

--- robin_tryout.py
+++ robin_tryout.py
@@ -20,76 +20,88 @@ MAX_STEPS = 20000
 
 INITIAL_ACCOUNT_BALANCE = 10000
 
-class StockTradingEnvironment(gym.Env):
+class BuyerEnvironment(gym.Env):
     """A stock trading environment for OpenAI gym"""
     metadata = {'render.modes': ['human']}
-    def __init__(self, df):
-        super(StockTradingEnvironment, self).__init__()
+    def __init__(self, df, properties):
+        super(BuyerEnvironment, self).__init__()
         self.df = df
+        self.properties = properties
         self.reward_range = (0, MAX_ACCOUNT_BALANCE)
-        # Actions of the format Buy x%, Sell x%, Hold, etc.
-        self.action_space = spaces.Box(low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
+
+        # Actions of the format Buy x%, or refrain, etc.
+        self.action_space = spaces.Box(low=np.array([0, 0]), high=np.array([2, 1000000]))
         # Prices contains the OHCL values for the last five prices
-        self.observation_space = spaces.Box(low=0, high=1, shape=(21, 6), dtype=np.float16)
+
+        self.lookback_period = 5
+        self.observation_space = spaces.Box(low=0, high=1, shape=(21, self.lookback_period + 1), dtype=np.float16)
+
+        self.product_shelf_life = properties['product_shelf_life']
+        self.ordering_cost = properties['ordering_cost']
+        self.storage_capacity = properties['storage_capacity']
+        self.min_inventory_threshold = properties['min_inventory_threshold']
+        self.consumption_rate = properties['consumption_rate']
+        self.storage_cost = properties['storage_cost']
+        self.cash_inflow = properties['cash_inflow']
 
     def reset(self):
         # Reset the state of the environment to an initial state
         self.balance = INITIAL_ACCOUNT_BALANCE
         self.net_worth = INITIAL_ACCOUNT_BALANCE
         self.max_net_worth = INITIAL_ACCOUNT_BALANCE
-        self.shares_held = 0
+        self.current_inventory = 0
         self.cost_basis = 0
         self.total_shares_sold = 0
         self.total_sales_value = 0
 
         # Set the current step to a random point within the data frame
-        self.current_step = random.randint(0, len(self.df.loc[:, 'y'].values) - 6)
+        self.current_step = random.randint(0, len(self.df.loc[:, 'y'].values) - (self.lookback_period + 1))
         return self._next_observation()
 
 
     def _next_observation(self):
-        # Get the data points for the last 5 days and scale to between 0-1
+        # Get the data points for the last 5 weeks and scale to between 0-1
         frame = np.array([
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_24_quo'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_24_quo'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_26_quo'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_26_quo'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_37_quo'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_37_quo'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_94_quo'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_94_quo'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_20_quo'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_20_quo'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_6_quo'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_6_quo'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_227_pro'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_227_pro'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'y_785_end'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'y_785_end'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'ma4'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'ma4'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'var4'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'var4'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'momentum0'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'momentum0'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'rsi'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'rsi'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'MACD'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'MACD'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'upper_band'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'upper_band'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'ema'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'ema'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'diff4'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'diff4'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'lower_band'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'lower_band'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'momentum1'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'momentum1'].values / MAX_SHARE_PRICE,
             self.df.loc[self.current_step: self.current_step +
-                                           5, 'kalman'].values / MAX_SHARE_PRICE,
+                                           self.lookback_period, 'kalman'].values / MAX_SHARE_PRICE,
         ])
 
 
@@ -97,12 +109,13 @@ class StockTradingEnvironment(gym.Env):
         obs = np.append(frame, [[
             self.balance / MAX_ACCOUNT_BALANCE,
             self.max_net_worth / MAX_ACCOUNT_BALANCE,
-            self.shares_held / MAX_NUM_SHARES,
+            self.current_inventory / MAX_NUM_SHARES,
             self.cost_basis / MAX_SHARE_PRICE,
             self.total_shares_sold / MAX_NUM_SHARES,
             self.total_sales_value / (MAX_NUM_SHARES * MAX_SHARE_PRICE),
         ]], axis=0)
-        
+
+
         return obs
 
     def step(self, action):
@@ -125,33 +138,54 @@ class StockTradingEnvironment(gym.Env):
         action_type = action[0]
         amount = action[1]
 
+        # Buy amount of product
         if action_type < 1:
-            # Buy amount % of balance in shares
-            total_possible = int(self.balance / current_price)
-            shares_bought = int(total_possible * amount)
-            prev_cost = self.cost_basis * self.shares_held
-            additional_cost = shares_bought * current_price
 
+            # determine max amount of buyable product (storage and cash constraints)
+            total_possible_cash = int(self.balance / current_price)
+            total_possible_storage = self.storage_capacity - self.current_inventory
+
+            total_possible = min([total_possible_storage, total_possible_cash])
+
+            # buy the amount, if too much, buy max amount
+            if amount < total_possible:
+                shares_bought = amount
+            else:
+                shares_bought = total_possible
+
+            # calculate average buying price of previous products
+            prev_cost = self.cost_basis * self.current_inventory
+
+            # calculate cost of current acquisition
+            additional_cost = shares_bought * current_price + shares_bought * self.ordering_cost
+
+            # update balance
             self.balance -= additional_cost
+
+            # calculate average buying price of all products
             self.cost_basis = (
-                prev_cost + additional_cost) / (self.shares_held + shares_bought)
-            self.shares_held += shares_bought
+                prev_cost + additional_cost) / (self.current_inventory + shares_bought)
 
+            # update inventory
+            self.current_inventory += shares_bought
+
+        # don't buy
         elif action_type < 2:
-            # Sell amount % of shares held
-            shares_sold = int(self.shares_held * amount)
-            self.balance += shares_sold * current_price
-            self.shares_held -= shares_sold
-            self.total_shares_sold += shares_sold
-            self.total_sales_value += shares_sold * current_price
+            pass
 
-        self.net_worth = self.balance + self.shares_held * current_price
+        self.net_worth = self.balance + self.current_inventory * current_price
 
         if self.net_worth > self.max_net_worth:
             self.max_net_worth = self.net_worth
 
-        if self.shares_held == 0:
+        if self.current_inventory == 0:
             self.cost_basis = 0
+
+        # update inventory with spoiled and used product
+        self.current_inventory -= self.consumption_rate
+
+        # update balance with operational costs
+        self.balance += self.cash_inflow - self.storage_cost * self.current_inventory
 
     def render(self, mode='human', close=False):
         # Render the environment to the screen
@@ -160,7 +194,7 @@ class StockTradingEnvironment(gym.Env):
         print(f'Step: {self.current_step}')
         print(f'Balance: {self.balance}')
         print(
-            f'Shares held: {self.shares_held} (Total sold: {self.total_shares_sold})')
+            f'Shares held: {self.current_inventory} (Total sold: {self.total_shares_sold})')
         print(
             f'Avg cost for held shares: {self.cost_basis} (Total sales value: {self.total_sales_value})')
         print(
@@ -173,10 +207,26 @@ if __name__ == '__main__':
 
     df = pd.read_csv('./data/US_SMP_food_TA.csv').iloc[69:].reset_index()
     df = df.sort_values('ds')
+
+    # define buyer properties
+    properties = {
+        'product_shelf_life': 8,
+        'ordering_cost': 0.5,
+        'storage_capacity': 10000,
+        'min_inventory_threshold': 2000,
+        'consumption_rate': 1000,
+        'storage_cost': 1,
+        'cash_inflow': 10000
+    }
+
     # The algorithms require a vectorized environment to run
-    env = DummyVecEnv([lambda: StockTradingEnvironment(df)])
+    env = DummyVecEnv([lambda: BuyerEnvironment(df, properties)])
     model = PPO("MlpPolicy", env, verbose=20)
+
+    # train model
     model.learn(total_timesteps=100000)
+
+    # carry out simulation
     obs = env.reset()
     for i in range(4000):
         action, _states = model.predict(obs)
