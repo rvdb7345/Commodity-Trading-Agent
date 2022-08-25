62870a7db0e4c8f05c46e9a8bdc8248f35e0b5f6
rvdb7345 <rvdb7345@gmail.com>
2022-08-25 11:11:22 +0200

    changed the features to list

--- robin_tryout.py
+++ robin_tryout.py
@@ -2,7 +2,6 @@ import gym
 import json
 import random
 
-
 import numpy as np
 import datetime as dt
 import pandas as pd
@@ -22,18 +21,20 @@ MAX_STEPS = 20000
 
 INITIAL_ACCOUNT_BALANCE = 10000
 
+
 class BuyerEnvironment(gym.Env):
     """A stock trading environment for OpenAI gym"""
     metadata = {'render.modes': ['human']}
-    def __init__(self, df, properties):
+
+    def __init__(self, df, properties, ts_feature_names):
         super(BuyerEnvironment, self).__init__()
         self.df = df
+        self.ts_feature_names = ts_feature_names
         self.properties = properties
         self.reward_range = (0, MAX_ACCOUNT_BALANCE)
 
         # Actions of the format Buy x%, or refrain, etc.
         self.action_space = spaces.Box(low=0, high=100000, shape=(1,))
-
 
         self.lookback_period = 20
         # self.observation_space = spaces.Box(low=0, high=1, shape=(21, self.lookback_period + 1), dtype=np.float16)
@@ -62,61 +63,63 @@ class BuyerEnvironment(gym.Env):
         self.current_step = random.randint(0, len(self.df.loc[:, 'y'].values) - (self.lookback_period + 1))
         return self._next_observation()
 
-
     def _next_observation(self):
         # Get the data points for the last 'look_back' weeks and scale to between 0-1
-        frame = {'time_series':[
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_24_quo'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_26_quo'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_37_quo'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_94_quo'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_20_quo'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_6_quo'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_227_pro'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['y_785_end'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['ma4'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['var4'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['momentum0'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['rsi'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['MACD'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['upper_band'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['ema'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['diff4'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['lower_band'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['momentum1'].values / MAX_PRODUCT_PRICE,
-            self.df.iloc[self.current_step: self.current_step +
-                                           self.lookback_period, :]['kalman'].values / MAX_PRODUCT_PRICE,
-        ]}
+        # frame = {'time_series':[
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_24_quo'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_26_quo'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_37_quo'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_94_quo'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_20_quo'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_6_quo'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_227_pro'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['y_785_end'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['ma4'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['var4'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['momentum0'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['rsi'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['MACD'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['upper_band'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['ema'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['diff4'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['lower_band'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['momentum1'].values / MAX_PRODUCT_PRICE,
+        #     self.df.iloc[self.current_step: self.current_step +
+        #                                    self.lookback_period, :]['kalman'].values / MAX_PRODUCT_PRICE,
+        # ]}
 
-
-        # Append additional data and scale each value to between 0-1
-        frame['env_props'] = [
+        frame = {'time_series': [
+            self.df.iloc[self.current_step: self.current_step + self.lookback_period, :][feat_name].values
+            for feat_name in self.ts_feature_names
+            ],
+            'env_props': [
             self.balance / MAX_ACCOUNT_BALANCE,
             self.current_inventory / MAX_NUM_PRODUCT,
             self.cost_basis / MAX_PRODUCT_PRICE,
             self.total_spent_value / (MAX_NUM_PRODUCT * MAX_PRODUCT_PRICE),
-        ]
+        ]}
 
+        # Append additional data and scale each value to between 0-1
 
         return frame
 
@@ -139,7 +142,7 @@ class BuyerEnvironment(gym.Env):
             reward -= 100000
 
         obs = self._next_observation()
-        done = False #TODO: change
+        done = False  # TODO: change
 
         return obs, reward, done, {}
 
@@ -149,7 +152,6 @@ class BuyerEnvironment(gym.Env):
 
         amount = action
         # print(amount)
-
 
         # determine max amount of buyable product (storage and cash constraints)
         total_possible_cash = int(self.balance / current_price)
@@ -167,7 +169,7 @@ class BuyerEnvironment(gym.Env):
 
         # calculate average buying price of all products
         self.cost_basis = (
-            prev_cost + additional_cost) / (self.current_inventory + product_bought)
+                                  prev_cost + additional_cost) / (self.current_inventory + product_bought)
 
         # update inventory
         self.current_inventory += product_bought
@@ -185,7 +187,7 @@ class BuyerEnvironment(gym.Env):
 
         # update balance with operational costs
         self.balance += self.cash_inflow - self.storage_cost * self.current_inventory
-        
+
     def render(self, mode='human', close=False):
         # Render the environment to the screen
 
@@ -196,7 +198,6 @@ class BuyerEnvironment(gym.Env):
 
 
 if __name__ == '__main__':
-
 
     df = pd.read_csv('./data/US_SMP_food_TA.csv', index_col=0).iloc[69:].reset_index(drop=True)
     df = df.sort_values('ds')
@@ -212,13 +213,16 @@ if __name__ == '__main__':
         'cash_inflow': 100000
     }
 
+    ts_feature_names = \
+        ["y", "y_24_quo", "y_26_quo", "y_37_quo", "y_94_quo", "y_20_quo", "y_6_quo", "y_227_pro", "y_785_end",
+         "ma4", "var4", "momentum0", "rsi", "MACD", "upper_band", "ema", "diff4", "lower_band", "momentum1", "kalman"]
+
     # The algorithms require a vectorized environment to run
-    env = DummyVecEnv([lambda: FlattenObservation(BuyerEnvironment(df, properties))])
+    env = DummyVecEnv([lambda: FlattenObservation(BuyerEnvironment(df, properties, ts_feature_names))])
 
     # specify the model used for learning a policy
     # model = RecurrentPPO("MlpLstmPolicy", env, verbose=20)
     model = PPO('MlpPolicy', env, verbose=20)
-
 
     # train model
     model.learn(total_timesteps=40000)
